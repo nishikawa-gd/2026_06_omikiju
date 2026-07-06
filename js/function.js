f560ece9ba499deac67c1f4f73551c1dc3048ae7
h.miki <kd1381066@st.kobedenshi.ac.jp>
2026-07-06 12:10:16 +0900

    画像サイズの調整

--- js/function.js
+++ js/function.js
@@ -1,6 +1,6 @@
 
 
-$(function() {
+$(function () {
 	function init() {
 		handlePlayPage();
 		handleResultPage();
@@ -19,7 +19,7 @@ function handlePlayPage() {
 	if (!$('body').hasClass('play')) return;
 
 	// show-resultボタン押下時の処理
-	$('#show-result').on('click', function(e) {
+	$('#show-result').on('click', function (e) {
 
 		// デフォルトの動作をキャンセル
 		e.preventDefault();
@@ -34,7 +34,7 @@ function handlePlayPage() {
 
 		// 履歴保存
 		const history = JSON.parse(localStorage.getItem('omikujiHistory') || '[]');
-		
+
 		// 履歴に追加
 		history.push({ result, lucky, date: new Date().toLocaleString() });
 		// 履歴をlocalStorageに保存
@@ -51,7 +51,7 @@ function handlePlayPage() {
 
 // ===== 結果ページの処理 =====
 function handleResultPage() {
-	
+
 	// bodyにresultクラスがなければ終了
 	if (!$('body').hasClass('result')) return;
 
@@ -63,7 +63,10 @@ function handleResultPage() {
 	// 結果表示
 	$('#result-text').text(`${data.result.type}：${data.result.text}`);
 	// 画像の設定
-	$('#result-img').attr('src', data.result.img).attr('alt', data.result.type);
+	$('#result-img')
+		.attr('src', data.result.img)
+		.attr('alt', data.result.type)
+		.addClass('result_img');
 
 	// 結果に応じたクラスをbodyに追加	
 	$('body').addClass('js-' + data.result.type);
@@ -96,9 +99,9 @@ function handleHistoryPage() {
 		);
 		$list.append($li);
 	});
-	
+
 	// 履歴削除ボタン処理
-	$('#clear-history').on('click', function() {
+	$('#clear-history').on('click', function () {
 		if (confirm('履歴をすべて削除しますか？')) {
 			localStorage.removeItem('omikujiHistory');
 			$list.empty();
--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,29 @@
 // おみくじ結果リスト
 const omikujiResults = [
-  { type: "大吉", text: "最高の一日になります！", img: "images/result/01_daikichi.png" },
-  { type: "中吉", text: "いいことがあるかも！", img: "images/result/02_chukichi.png" },
-  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/03_shokichi.png" },
-  { type: "凶", text: "気をつけて過ごしましょう", img: "images/result/04_kyou.png" }
+  {
+    type: "大吉",
+    text: "最高の一日になります！",
+    img: "images/result/01_daikichi.png",
+    class: "result_img"
+  },
+  {
+    type: "中吉",
+    text: "いいことがあるかも！",
+    img: "images/result/02_chukichi.png",
+    class: "result_img"
+  },
+  {
+    type: "小吉",
+    text: "少し嬉しい出来事が…",
+    img: "images/result/03_shokichi.png",
+    class: "result_img"
+  },
+  {
+    type: "凶",
+    text: "気をつけて過ごしましょう",
+    img: "images/result/04_kyou.png",
+    class: "result_img"
+  }
 ];
 
 // ラッキーアイテム
