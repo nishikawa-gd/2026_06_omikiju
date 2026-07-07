60fc422ecfc3720a6871c4058b26115a91b29a34
木内　洋輝 <>
2026-07-07 09:03:39 +0900

    画像変えた

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,17 @@
 // おみくじ結果リスト
 const omikujiResults = [
-  { type: "大吉", text: "最高の一日になります！", img: "images/result/daikichi.png" },
-  { type: "中吉", text: "いいことがあるかも！", img: "images/result/chukichi.png" },
-  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/shokichi.png" },
-  { type: "凶", text: "気をつけて過ごしましょう", img: "images/result/kyo.png" }
+  { type: "大吉", text: "最高の一日になります！", img: "images/result/nonn08.png" },
+  { type: "中吉", text: "いいことがあるかも！", img: "images/result/nonn05.png" },
+  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/nonn06.png" },
+  { type: "末吉", text: "少し嬉しい出来事が…", img: "images/result/nonn02.png" },
+  { type: "吉", text: "少し嬉しい出来事が…", img: "images/result/nonn07.png" },
+  { type: "ふん吉", text: "少し嬉しい出来事が…", img: "images/result/nonn03.png" },
+  { type: "クロ吉", text: "少し嬉しい出来事が…", img: "images/result/nonn01.png" },
+  { type: "大凶", text: "気をつけて過ごしましょう", img: "images/result/nonn04.png" }
 ];
 
 // ラッキーアイテム
-const luckyItems = ["傘", "本", "スマホ", "鍵", "花", "お茶"];
+const luckyItems = ["鈴原希1st写真集「届け」", "フライドポテト", "パン屋さんのパン", "工場見学", "ラブライブ!スーパースター!Blu-ray-BOX", "ペットボトルオープナー"];
 
 // ラッキーアイテムON/OFF
 const settings = {
