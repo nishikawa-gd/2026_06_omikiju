6df5afc8a393ac0588bdda255773eb830055b645
木内　洋輝 <>
2026-07-12 17:44:36 +0900

    完成

--- js/settings.js
+++ js/settings.js
@@ -1,17 +1,17 @@
 // おみくじ結果リスト
 const omikujiResults = [
   { type: "大吉", text: "ミュージックワールドに行けるかも！？", img: "images/result/nonn08.png" },
-  { type: "中吉", text: "いいことがあるかも！", img: "images/result/nonn05.png" },
-  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/nonn06.png" },
-  { type: "末吉", text: "少し嬉しい出来事が…", img: "images/result/nonn02.png" },
-  { type: "吉", text: "少し嬉しい出来事が…", img: "images/result/nonn07.png" },
-  { type: "ふん吉", text: "少し嬉しい出来事が…", img: "images/result/nonn03.png" },
-  { type: "クロ吉", text: "少し嬉しい出来事が…", img: "images/result/nonn01.png" },
-  { type: "大凶", text: "気をつけて過ごしましょう", img: "images/result/nonn04.png" }
+  { type: "中吉", text: "お祭りに女神が現れる！？", img: "images/result/nonn05.png" },
+  { type: "小吉", text: "ある日くまさんに出会っちゃうかも！？", img: "images/result/nonn06.png" },
+  { type: "末吉", text: "真夏に海でもいっちゃう？", img: "images/result/nonn02.png" },
+  { type: "吉", text: "白猫を見つけたら幸運が舞い込む！？", img: "images/result/nonn07.png" },
+  { type: "ふん吉", text: "今日はちょっと違う道を冒険してみよう！", img: "images/result/nonn03.png" },
+  { type: "クロ吉", text: "夜羽咲クロネ様の配信を見れば１日安泰！", img: "images/result/nonn01.png" },
+  { type: "大凶", text: "言葉かみかみのお口弱々dayかも…", img: "images/result/nonn04.png" }
 ];
 
 // ラッキーアイテム
-const luckyItems = ["鈴原希1st写真集「届け」", "フライドポテト", "パン屋さんのパン", "工場見学", "ラブライブ!スーパースター!Blu-ray-BOX", "ペットボトルオープナー"];
+const luckyItems = ["鈴原希実1st写真集「届け」", "フライドポテト", "パン屋さんのクロワッサン", "工場見学", "ラブライブ!スーパースター!Blu-ray-BOX", "ペットボトルオープナー"];
 
 // ラッキーアイテムON/OFF
 const settings = {
