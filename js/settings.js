1e147ef891ad0b308f5097fe57b8aa912894decc
古田芽唯 <>
2026-07-13 11:41:53 +0900

    js書き換え

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,26 @@
 // おみくじ結果リスト
 const omikujiResults = [
-  { type: "大吉", text: "最高の一日になります！", img: "images/result/daikichi.png" },
-  { type: "中吉", text: "いいことがあるかも！", img: "images/result/chukichi.png" },
-  { type: "小吉", text: "少し嬉しい出来事が…", img: "images/result/shokichi.png" },
-  { type: "凶", text: "気をつけて過ごしましょう", img: "images/result/kyo.png" }
+  { type: "大吉", text: "お釣りが777円になるかも!?記念にレシートは取っておこう!", img: "images/result/daikichi.png" },
+  { type: "中吉", text: "新商品との運命の出会いあり！買わないと後悔するかも。", img: "images/result/chukichi.png" },
+  { type: "小吉", text: "温かいものを食べると午後の眠気が少しだけマシになる…かも?", img: "images/result/shokichi.png" },
+  { type: "凶", text: "食べたいと思った商品が売り切れている予感…", img: "images/result/kyo.png" },
+  { type: "大凶", text: "レジ直前で財布を忘れたことに気付く。", img: "images/result/daikyo.png" }
 ];
 
+
 // ラッキーアイテム
-const luckyItems = ["傘", "本", "スマホ", "鍵", "花", "お茶"];
+const luckyItems = [
+  " ツナマヨおにぎり +  緑茶 +  プリン",
+  " たまごサンド +  カフェラテ +  クッキー",
+  " のり弁当 +  麦茶 +  チョコ",
+  " ナポリタン +  オレンジジュース +  シュークリーム",
+  " カップ麺 +  牛乳 +  アイス",
+  " からあげ棒 +  鮭おにぎり +  コーラ",
+  " アメリカンドッグ +  コーヒー +  ドーナツ",
+  " 肉まん +  ほうじ茶 +  杏仁豆腐",
+  " カレー +  ウーロン茶 +  バニラアイス",
+  " ピザまん +  アップルジュース +  グミ"
+];
 
 // ラッキーアイテムON/OFF
 const settings = {
